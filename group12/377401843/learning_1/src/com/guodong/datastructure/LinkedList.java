3ce3d830ac760530a9ace0a69fd349c2f40d5107
guodongym <guodongym@163.com>
2017-02-22 23:24:47 +0800

    linkedList
    linkedList Add实现

--- group12/377401843/learning_1/src/com/guodong/datastructure/LinkedList.java
+++ group12/377401843/learning_1/src/com/guodong/datastructure/LinkedList.java
@@ -1,45 +1,68 @@
 package com.guodong.datastructure;
 
 public class LinkedList implements List {
-	
+
+	private int size = 0;
+
 	private Node head;
-	
-	public void add(Object o){
-		
+
+	private Node last;
+
+	public void add(Object o) {
+		if (head == null) {
+			head = new Node(o, null);
+			last = new Node(o, null);
+		} else {
+			Node n = last;
+			last = new Node(o, null);
+			n.next = last;
+		}
+		size++;
 	}
-	public void add(int index , Object o){
-		
+
+	public void add(int index, Object o) {
+
 	}
-	public Object get(int index){
+
+	public Object get(int index) {
 		return null;
 	}
-	public Object remove(int index){
+
+	public Object remove(int index) {
 		return null;
 	}
-	
-	public int size(){
-		return -1;
+
+	public int size() {
+		return size;
 	}
-	
-	public void addFirst(Object o){
-		
+
+	public void addFirst(Object o) {
+
 	}
-	public void addLast(Object o){
-		
+
+	public void addLast(Object o) {
+
 	}
-	public Object removeFirst(){
+
+	public Object removeFirst() {
 		return null;
 	}
-	public Object removeLast(){
+
+	public Object removeLast() {
 		return null;
 	}
-	public Iterator iterator(){
+
+	public Iterator iterator() {
 		return null;
 	}
-	
-	
-	private static  class Node{
+
+	private static class Node {
 		Object data;
 		Node next;
+
+		public Node(Object data, Node next) {
+			this.data = data;
+			this.next = next;
+		}
 	}
 }
